524d3b2ad6a4a1b0f81a0d5744545128bd357dc9
tobymiles <ekwealortobechukwu22@gmail.com>
2023-08-10 14:54:59 +0100

    video added

--- src/Components/Landingsection.js
+++ src/Components/Landingsection.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./Landingsection.css";
+// import ReactPlayer from 'react-player';
+import videoFile from "../video.mp4"
 // import Slider from "react-slick";
 
-import Landingpageimg1 from "../Assets/img1.jpg";
-import Scrollimg1 from "../Assets/scroll-down-arrow.gif";
+// import Landingpageimg1 from "../Assets/img1.jpg";
+// import Scrollimg1 from "../Assets/scroll-down-arrow.gif";
 
 function Landingsection() {
   return (
     <div className='Hero'>
+       <video className="back-video" loop muted autoPlay playsInline>
+        <source src={videoFile} type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
       <div className='floating-card'>
         <h1>We Create</h1>
         <p id='Hero-paragrah'>Lasting Impression</p>
       </div>
-      <img src={Landingpageimg1} id='landingpageimg' alt='landingpageimg' />
-      <img src={Scrollimg1} id='Scrollimg' alt='Scrollimg' />
+      {/* <img src={Landingpageimg1} id='landingpageimg' alt='landingpageimg' /> */}
+      {/* <img src={Scrollimg1} id='Scrollimg' alt='Scrollimg' /> */}
     </div>
   );
 }
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Contact.css"
 
 function Contact() {
-  return <div>Contact</div>;
+  return <div className="contact-heading">
+  <div className="content">
+  <h1>Contact</h1>
+  </div>
+  </div>;
 }
 
 export default Contact;
